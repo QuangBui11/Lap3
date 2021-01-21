8bb40387b5c982973017a77e7603705f790916f1
QuangBui11 <77773344+QuangBui11@users.noreply.github.com>
2021-01-21 15:12:30 +0700

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,4 +1,11 @@
+<html>
+  <head>
+    <title>PHP TEST</title>
+  </head>
+  <body>
 <?php
   $name='NIIT - ICT Hà Nội';
   echo "$name là đơn vị chuyên đào tạo Lập trình viên chất lượng cao";
 ?>
+  </body>
+</html>

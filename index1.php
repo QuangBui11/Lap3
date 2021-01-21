18cb73e99d86298ce16270d8437eaf24f1789d3f
QuangBui11 <77773344+QuangBui11@users.noreply.github.com>
2021-01-21 15:04:56 +0700

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,8 +1,4 @@
-Hello world! This is my first PHP script.
 <?php
-echo "Hello world, PHP\r\n";
-$name = readline('Your name: ');
-echo "Hello, $name. Welcome to heaven!\r\n";
+  $name='NIIT - ICT Hà Nội';
+  echo "$name là đơn vị chuyên đào tạo Lập trình viên chất lượng cao";
 ?>
-Hello again, <?php echo $name ?>. This text is not in the php tags.
-Hi, <?=$name?>. We meet again!

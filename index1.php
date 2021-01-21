3d7cad48e940caec61f9f65ccb9b2b1afb113115
QuangBui11 <77773344+QuangBui11@users.noreply.github.com>
2021-01-21 14:56:16 +0700

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,8 @@
+Hello world! This is my first PHP script.
+<?php
+echo "Hello world, PHP\r\n";
+$name = readline('Your name: ');
+echo "Hello, $name. Welcome to heaven!\r\n";
+?>
+Hello again, <?php echo $name ?>. This text is not in the php tags.
+Hi, <?=$name?>. We meet again!
